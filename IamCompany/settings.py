4c038d8c03bb13b704ef37e58aea8d8bf7484556
P1ecful <p1ecfuldog@gmail.com>
2023-06-29 16:00:37 +0300

    optization #2 |  add cache

--- IamCompany/settings.py
+++ IamCompany/settings.py
@@ -96,3 +96,10 @@ REST_FRAMEWORK = {
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'cache'),
+    }
+}
